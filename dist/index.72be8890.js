9b258ede40f6cd5c9eb9d0cfc87e79aa19c4036e
Matt Daniel Brown <developer@mattdanielbrown.com>
2023-02-19 23:21:58 -0600

    Added debouncing window width check for mobile navigation

--- dist/index.72be8890.js
+++ dist/index.72be8890.js
@@ -1,10 +1,28 @@
 "use strict";
 function main() {
     console.log("MAIN function has started...");
+    let timeout = false, delay = 250, calls = 0;
+    // window.resize callback function
+    function getDimensions() {
+        let screenWidth = window.innerWidth;
+        calls += 1;
+        if (screenWidth >= 600) {
+            if (mobileNavigationIsActive === true) {
+                mobileNavigationIsActive = false;
+                mobileNavMenuButton.setAttribute("aria-expanded", "false");
+                mobileNavMenuButtonTextValue.innerHTML = "Menu";
+                primaryNavigation.classList.remove("active");
+            }
+        }
+    }
     const mobileNavMenuButton = document.getElementById("mobile-navigation-control");
     const navMenu = document.getElementById("navigation-menu");
     const primaryNavigation = document.getElementById("primary-navigation");
     const mobileNavMenuButtonTextValue = document.querySelector(".mobile-navigation-control-text-content");
+    let mobileNavigationIsActive = false;
+    function toggleMobileNavigationIsActive() {
+        mobileNavigationIsActive = mobileNavigationIsActive !== true;
+    }
     function toggleAriaExpandedOfElement(targetElement) {
         if (targetElement.getAttribute("aria-expanded") === "true") targetElement.setAttribute("aria-expanded", "false");
         else targetElement.setAttribute("aria-expanded", "true");
@@ -20,13 +38,20 @@ function main() {
         primaryNavigation.classList.toggle("active");
     }
     mobileNavMenuButton.addEventListener("click", function() {
-        console.log("menu button clicked...");
+        toggleMobileNavigationIsActive();
         toggleAriaExpandedOfElement(this);
         toggleTextValueOfElement(mobileNavMenuButtonTextValue);
         // toggleActiveClassForNavigationMenu();
         toggleActiveClassForNavigation();
     });
     document.addEventListener("touchstart", function() {}, true);
+    // window.resize event listener
+    window.addEventListener("resize", function() {
+        // clear the timeout
+        clearTimeout(timeout);
+        // start timing for event "completion"
+        timeout = setTimeout(getDimensions, delay);
+    });
 }
 window.addEventListener("DOMContentLoaded", ()=>{
     console.log("DOM fully loaded and parsed.");
--- js/main.js
+++ js/main.js
@@ -3,12 +3,37 @@
 function main() {
 	console.log('MAIN function has started...');
 	
+	
+	let timeout = false, // holder for timeout id
+			delay   = 250, // delay after event is "complete" to run callback
+			calls   = 0;
+
+	// window.resize callback function
+	function getDimensions() {
+		let screenWidth = window.innerWidth;
+		calls += 1;
+		if (screenWidth >= 600) {
+			if (mobileNavigationIsActive === true) {
+				mobileNavigationIsActive = false;
+				mobileNavMenuButton.setAttribute('aria-expanded', 'false');
+				mobileNavMenuButtonTextValue.innerHTML = "Menu";
+				primaryNavigation.classList.remove('active');
+			}
+		}
+	}
+
+	
+	
 	const mobileNavMenuButton = document.getElementById('mobile-navigation-control');
 	const navMenu = document.getElementById('navigation-menu');
 	const primaryNavigation = document.getElementById('primary-navigation');
 	const mobileNavMenuButtonTextValue = document.querySelector('.mobile-navigation-control-text-content');
 	
+	let mobileNavigationIsActive = false;
 	
+	function toggleMobileNavigationIsActive() {
+		mobileNavigationIsActive = mobileNavigationIsActive !== true;
+	}
 	function toggleAriaExpandedOfElement(targetElement) {
 		if (targetElement.getAttribute('aria-expanded') === 'true') {
 			targetElement.setAttribute('aria-expanded', 'false');
@@ -31,7 +56,7 @@ function main() {
 	}
 	
 	mobileNavMenuButton.addEventListener('click', function () {
-		console.log("menu button clicked...");
+		toggleMobileNavigationIsActive();
 		toggleAriaExpandedOfElement(this);
 		toggleTextValueOfElement(mobileNavMenuButtonTextValue);
 		// toggleActiveClassForNavigationMenu();
@@ -39,6 +64,14 @@ function main() {
 	});
 	
 	document.addEventListener("touchstart", function () {}, true);
+	
+	// window.resize event listener
+	window.addEventListener('resize', function () {
+		// clear the timeout
+		clearTimeout(timeout);
+		// start timing for event "completion"
+		timeout = setTimeout(getDimensions, delay);
+	});
 }
 
 window.addEventListener('DOMContentLoaded', () => {
